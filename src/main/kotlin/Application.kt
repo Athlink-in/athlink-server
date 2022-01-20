044941cd70ae1c6f75ce9c39adcf08a3e342d040
Phaniraj Aenugula <paenugul@calpoly.edu>
2022-01-20 15:52:01 -0800

    initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Welcome to Athlink!")
+            }
+        }
+    }.start(wait = true)
+}
