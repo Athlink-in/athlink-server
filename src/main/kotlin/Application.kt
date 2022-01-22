48091b7a3444c1d9281465f58fc95b19cc382645
Ishaan Karvir <ikarvir@calpoly.edu>
2022-01-21 18:45:34 -0800

    Init

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,16 +1,37 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
+    infix fun String.ate(a: String) {
+        println("${this} just ate ${a}")
+    }
     embeddedServer(Netty, port = 8080) {
+        install(CORS) {
+            method(HttpMethod.Options)
+            method(HttpMethod.Put)
+            method(HttpMethod.Delete)
+            method(HttpMethod.Patch)
+            header(HttpHeaders.Authorization)
+            header(HttpHeaders.AccessControlAllowOrigin)
+            allowNonSimpleContentTypes = true
+            allowSameOrigin = true
+            host("*", listOf("http", "https"))
+            install(ContentNegotiation){
+                json()
+            }
+        }
         routing {
             get("/") {
-                call.respondText("Welcome to Athlink!")
+                println("GET Request")
+                call.respond(mapOf("user" to "test user"))
             }
         }
     }.start(wait = true)
