9270e6dd8c1584868c3ca67e3dfef7012325134a
Phaniraj Aenugula <paenugul@calpoly.edu>
2022-02-10 23:08:41 -0800

    adding attributes

--- src/main/kotlin/model/Profile.kt
+++ src/main/kotlin/model/Profile.kt
@@ -1,6 +1,7 @@
 package com.athlink.model
 
 import com.athlink.util.BSONTimestampSerializer
+import io.ktor.util.date.*
 import kotlinx.serialization.Serializable
 import org.bson.BsonTimestamp
 import org.litote.kmongo.Id
@@ -8,14 +9,15 @@ import org.litote.kmongo.newId
 
 @Serializable
 data class Profile(
-    var firstName: String?,
-    var lastName: String?,
+    var firstname: String?,
+    var lastname: String?,
     val height: Int?,
     val age: Int?,
     val school: String?,
     val weight: Int?,
-    val gradYear: Int?,
-    @Serializable(with = BSONTimestampSerializer::class) var memberSince: BsonTimestamp?,
+    val gradyear: Int?,
+    val sex: String?,
+    @Serializable(with = BSONTimestampSerializer::class) var memberSince: BsonTimestamp? = BsonTimestamp(getTimeMillis()),
     val email: String?,
     var _id: Id<Profile> = newId(),
 )
