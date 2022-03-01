40996cb3d5c01039d71b1b553b39bcd7d00277aa
Keerat_Grewal <kgrewa03@calpoly.edu>
2022-02-28 23:49:57 -0800

    fixed bug updated script

--- generateData.py
+++ generateData.py
@@ -1,8 +1,11 @@
 import requests
 import random
 import randomname
+<<<<<<< HEAD
 from youtubesearchpython import VideosSearch
 
+=======
+>>>>>>> d740b5f (updated script)
 
 def generateData():
     emails = ["phanirajaenugula@gmail.com", "brandonburana2@gmail.com", "karvirishaan@gmail.com",
@@ -11,6 +14,7 @@ def generateData():
 
     for i in range(2):
 
+<<<<<<< HEAD
         curr_user = emails[random.randint(0, len(emails)-1)]
         curr_tags = [tags[random.randint(0, len(tags)-1)]]
         videosSearch = VideosSearch(curr_tags[0], limit = 5)
@@ -33,8 +37,43 @@ def generateData():
                 "postId": "test"
             }
             print(requests.post("https://athlink-server.herokuapp.com/post", json=data).status_code)
+=======
+    items = []
 
+    for _ in range(1):
+        for _ in range(3):
+            items.append(randomname.generate(('n/sports')))
+        data = {
+            "userEmail": emails[random.randint(0, len(emails)-1)],
+            "postContent": "random thing",
+            "linkUrl": "",
+            "photoUrl": "/static/media/brandon.5c762569341bd800f8da.png",
+            "tags": items,
+            "timePosted": None,
+            "likes": 0,
+            "userName": 'testing'
+>>>>>>> d740b5f (updated script)
+
+        }
+        res = requests.post("http://athlink-server.herokuapp.com/post", json=data)
+        print(res.status_code)
+    print(items)
 
 generateData()
 
+<<<<<<< HEAD
 
+=======
+# var postContent: String?,
+# var timePosted: String?,
+# val likes: Int?,
+# var linkUrl: String?,
+# var tags: List<String>?,
+# var userEmail: String?,
+# var photoUrl: String? = null,
+# var userName: String? = null
+
+
+res = requests.get("http://athlink-server.herokuapp.com/post", params={'tag': 'hockey'})
+print(res.text)
+>>>>>>> d740b5f (updated script)
