279f002e9b6b548a49a1f41f046da7b75e9abd35
PAenugula <phanirajaenugula@gmail.com>
2022-03-01 00:28:18 -0800

    by user and trending

--- src/main/kotlin/api/PostManagement.kt
+++ src/main/kotlin/api/PostManagement.kt
@@ -11,30 +11,30 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import org.bson.BsonTimestamp
 import io.ktor.util.date.*
+import org.bson.conversions.Bson
 import org.litote.kmongo.*
 import kotlin.math.min
 
 
 fun Application.postManagementRoutes(db: AthlinkDatabase){
     routing {
-        route("/post"){
-            get{
-                val totalDocuments = db.posts.countDocuments()
+        route ("/post") {
+            get {
                 var limit = call.parameters["limit"]?.toInt() ?: 10;
-                limit = min(limit, totalDocuments.toInt())
                 val start = call.parameters["last_time"]?.toInt() ?: getTimeMillis();
                 val tag = call.parameters["tag"]
-                val posts = if (tag == null || tag.isBlank()) {
-                    db.posts.find(MongoPost::timePosted.lt(BsonTimestamp(start.toInt(), 1)))
+                val user = call.parameters["user"]
+                val filterList = emptyList<Bson>().toMutableList()
+
+                filterList += MongoPost::timePosted.lt(BsonTimestamp(start.toInt(), 1))
+
+                tag?.let { filterList += MongoPost::tags.contains(it) }
+                user?.let { filterList += MongoPost::userEmail.eq(it) }
+
+                val posts = db.posts.find(and(filterList)).sort(descending(MongoPost::timePosted))
                         .take(limit)
                         .map{ it.toJSPost() }
                         .toList()
-                } else {
-                    db.posts.find(and(MongoPost::timePosted.lt(BsonTimestamp(start.toInt(), 1)), MongoPost::tags.contains(tag)))
-                        .take(limit)
-                        .map{ it.toJSPost() }
-                        .toList()
-                }
                 posts.forEach {
                     val user = db.profiles.findOne(MongoProfile::email.eq(it.userEmail))
                     it.photoUrl = user?.photoURL
@@ -42,11 +42,25 @@ fun Application.postManagementRoutes(db: AthlinkDatabase){
                 }
                 call.respond(posts)
             }
-            post{
+            post {
                 val newPost = call.receive<JSPost>().toMongoPost()
                 newPost.timePosted = BsonTimestamp(System.currentTimeMillis().toInt(), 1)
                 db.posts.insertOne(newPost)
                 call.respond(HttpStatusCode.OK)
+            }
+            get ("/trending") {
+                val postTags = db.posts.find()
+                    .toList()
+                    .map { it.tags ?: emptyList() }
+                    .flatMap { it }
+                    .groupBy { it }
+                val frequencies = postTags
+                    .mapValues { it -> it.value.size }
+                    .toList()
+                    .sortedBy { (_, value) -> -value }
+                    .map { it.first }
+                    .take(10)
+                call.respond(frequencies)
             }
         }
     }
