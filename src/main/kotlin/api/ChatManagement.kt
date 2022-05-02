c8222c1c0b1c4bb006c86ba7b3c3ce95de6501a2
Ishaan Karvir <ikarvir@calpoly.edu>
2022-05-01 17:30:46 -0700

    Added get request to fetch previous chat, fixed bug with client side reload

--- src/main/kotlin/api/ChatManagement.kt
+++ src/main/kotlin/api/ChatManagement.kt
@@ -11,12 +11,15 @@ import java.util.*
 import kotlin.collections.LinkedHashSet
 import com.athlink.model.SocketConnection
 import com.google.gson.Gson
+import com.mongodb.client.model.Filters
 import io.ktor.http.*
+import io.ktor.response.*
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-
+import org.litote.kmongo.or
+import org.litote.kmongo.and
 
 
 fun Application.chatManagementRoutes(db: AthlinkDatabase){
@@ -40,27 +43,45 @@ fun Application.chatManagementRoutes(db: AthlinkDatabase){
         webSocket("/testWebsocket2/{email}"){
             val receiverEmail = call.parameters["email"].toString()
             println("Setting up server for $receiverEmail")
-            connections += SocketConnection(this, receiverEmail)
+            val connection = SocketConnection(this, receiverEmail)
+            println(connections)
+
+
+            if(connections.contains(connection)){
+                connections.remove(connection)
+            }
+            connections += connection
+            println(connections.size)
             try {
                 for (frame in incoming) {
                     frame as? Frame.Text ?: continue
                     val receivedText = frame.readText()
                     println(receivedText)
                     val message = Json.decodeFromString<JSMessage>(receivedText)
-                    val connection = connections.find { it.name == message.toEmail }
+                    println(message)
+                    val connection = connections.find{ it.name == message.toEmail }
+//                    connections.filter { it.name == message.toEmail }.forEach { it.session.send(Gson().toJson(message)) }
 //                    connections.find { it.name == message.toEmail }?.session?.send(Gson().toJson(message))
                     connection?.session?.send(Gson().toJson(message))
-
 
                     println("should add to db now")
                     print(message.toMongoMessage())
                     db.messages.insertOne(message.toMongoMessage())
                 }
-            } catch (e: ClosedReceiveChannelException) {
+            } catch (e : Exception) {
                 println("onClose ${closeReason.await()}")
-            } catch (e: Throwable) {
-                println("onError ${closeReason.await()}")
-                e.printStackTrace()
+                println("CLOSED")
+                connections -= connection
+            }
+        }
+        route("/messages") {
+            get("/{fromEmail}/{toEmail}") {
+                val fromEmail = call.parameters["fromEmail"].toString()
+                val toEmail = call.parameters["toEmail"].toString()
+                val messages = db.messages.find(and(or(Filters.eq("toEmail", fromEmail), Filters.eq("fromEmail", fromEmail)),
+                    or(Filters.eq("toEmail", toEmail), Filters.eq("fromEmail", toEmail))))
+                messages.forEach{ println(it.toJSMessage())}
+                call.respond(messages.map { it.toJSMessage() }.toList())
             }
         }
     }
--- src/main/kotlin/model/SocketConnections.kt
+++ src/main/kotlin/model/SocketConnections.kt
@@ -1,6 +1,7 @@
 package com.athlink.model
 
 import io.ktor.http.cio.websocket.*
+import io.ktor.network.sockets.*
 import io.ktor.websocket.*
 import java.util.concurrent.atomic.*
 
@@ -8,6 +9,17 @@ class SocketConnection(val session: DefaultWebSocketSession, private val email :
     val name = email
     companion object {
         var lastId = AtomicInteger(0)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is SocketConnection){
+            return other.name == name
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
     }
 
 }
